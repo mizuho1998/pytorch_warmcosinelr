3201cea575c782c915967e8cd33141a2ba325b54
mizuhof <moc.liam0000@gmail.com>
2020-08-29 21:43:37 +0900

    update test.py

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ model = torch.hub.load('pytorch/vision:v0.6.0', 'resnet18')
 optimizer = optim.SGD(model.parameters(), lr=0.1)
 
 epoch = 100
-scheduler = WarmCosineAnnealingLR(optimizer, epoch, 10, 1e-5)
+scheduler = WarmCosineAnnealingLR(optimizer, epoch, warmup=10, eta_min=1e-5)
 
 l = []
 for epoch in range(epoch):
